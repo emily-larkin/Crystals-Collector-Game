f87e4d1fa19a00f9e208610275ed9ef583ee633d
emssslay <emily.robinson@iechs.org>
2019-02-25 18:12:52 -0700

    pseudo coding

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// variables 
+var wins = 0;
+var losses = 0;
+var score = 0;
+var scoreCounter = 0;
+
+// create 4 crystal buttons that each have a different value
+
+
+// creates a random number for the goal for the user
+var goalScore = goalScore [Math.floor(Math.random())];
+
+// when the crystals are clicked, it adds their value to the users score
+
+
+// if the user meets the goalScore, the user wins
+
+
+// if the user goes above the goalScore, the user loses 
+
+
+// the game resets if the user wins or loses 
+
+
+// when the game resets, the user will see a new random number that is the goalScore
+
+
+// when the game resets, the value of each crystal will change
+
+
+// when the game resets, the users score and score counter will reset to 0
+
+
+// 
