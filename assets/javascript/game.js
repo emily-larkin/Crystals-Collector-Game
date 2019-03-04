a4f20e8361a1118bfabfffc79f70f544dd592fc7
emssslay <emily.robinson@iechs.org>
2019-03-04 14:23:54 -0700

    cleaning up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,12 +28,22 @@ function makeGoal() {
     goalScoreText.textContent = "Goal Score: " + goalScore;
 }
 
+var winsText = document.getElementById("wins-text");
+winsText.textContent = "Wins: " + wins;
+
+var lossesText = document.getElementById("losses-text");
+lossesText.textContent = "Losses: " + losses;
+
+var totalScoreText = document.getElementById("score-text");
+totalScoreText.textContent = "Your total score is: " + totalScore;
+
+
 // when the crystals are clicked, it adds their value to the users score
 function crystal1() {
     console.log(crystalValue1);
     totalScore = totalScore + crystalValue1;
     console.log(totalScore);
-    var totalScoreText = document.getElementById("score-text");
+    totalScoreText = document.getElementById("score-text");
     totalScoreText.textContent = "Your total score is: " + totalScore;
     checkScore(totalScore);
 }
@@ -42,7 +52,7 @@ function crystal2() {
     console.log(crystalValue2);
     totalScore = totalScore + crystalValue2;
     console.log(totalScore);
-    var totalScoreText = document.getElementById("score-text");
+    totalScoreText = document.getElementById("score-text");
     totalScoreText.textContent = "Your total score is: " + totalScore;
     checkScore(totalScore);
 }
@@ -50,7 +60,7 @@ function crystal2() {
 function crystal3() {
     console.log(crystalValue3);
     totalScore = totalScore + crystalValue3;
-    var totalScoreText = document.getElementById("score-text");
+    totalScoreText = document.getElementById("score-text");
     totalScoreText.textContent = "Your total score is: " + totalScore;
     console.log(totalScore);
     checkScore(totalScore);
@@ -59,18 +69,21 @@ function crystal3() {
 function crystal4() {
     console.log(crystalValue4);
     totalScore = totalScore + crystalValue4;
-    var totalScoreText = document.getElementById("score-text");
+    totalScoreText = document.getElementById("score-text");
     totalScoreText.textContent = "Your total score is: " + totalScore;
     console.log(totalScore);
     checkScore(totalScore);
 }
 
+
+// the game resets if the user wins or loses 
+// when the game resets, the user will see a new random number that is the goalScore
+// when the game resets, the users score and score counter will reset to 0
 function checkScore(totalScore){
     // if the user meets the goalScore, the user wins
     if (totalScore === goalScore) {
         wins++;
         alert("You win!");
-        displaytoPage(totalScore)
         gameRestart()
     }
     
@@ -78,13 +91,13 @@ function checkScore(totalScore){
     else if (totalScore >= goalScore) {
         losses = losses + 1;
         alert("You lose!");
-        displaytoPage(totalScore)
         gameRestart()
     }
-    var winsText = document.getElementById("wins-text");
-    winsText.textContent = "Wins: " + wins;
     
-    var lossesText = document.getElementById("losses-text");
+    winsText;
+    winsText.textContent = "Wins: " + wins;
+
+    lossesText;
     lossesText.textContent = "Losses: " + losses;
 }
 
@@ -93,26 +106,4 @@ function gameRestart(){
     makeGoal();
 }
 
-function displaytoPage(totalScore) {
-
-    
-}
-
 makeGoal();
-
-
-
-
-// the game resets if the user wins or loses 
-
-
-// when the game resets, the user will see a new random number that is the goalScore
-
-
-// when the game resets, the value of each crystal will change
-
-
-// when the game resets, the users score and score counter will reset to 0
-
-
-// show the number of games the user has won/lost
